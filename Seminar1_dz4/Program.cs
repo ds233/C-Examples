c552d11115114503cb97c4420aa53947f01400d5
Jack Sparrow <eugen-vorobiev@yandex.ru>
2022-08-24 22:32:22 +0300

    Написал программу по Домашнему заданию 4

---
+++ Seminar1_dz4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает три числа и выдает максимальное из этих чисел
+int num1, num2, num3;
+int max;
+
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+//Установим максимум на первое число
+max = num1;
+
+//Проверим не больше ли максимума второе и третье число и если больше, то установим новый максимум
+if(num2 > max){max = num2;}
+if(num3 > max){max = num3;}
+
+Console.WriteLine("Максимальное число: " + max);
+
