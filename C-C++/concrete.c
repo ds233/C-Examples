e17175f934b90e48d565b00f8a03d843632dcd13
Jack Sparrow <eugen-vorobiev@yandex.ru>
2022-10-17 10:47:55 +0300

    First programm on C

---
+++ C-C++/concrete.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (void)
+{
+    printf("Concrete has portland-cement and water.\n");
+    return 0;
+}
--- Seminar1_dz2/Program.cs
+++ Seminar1_dz2/Program.cs
@@ -7,6 +7,7 @@ num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 num2 = Convert.ToInt32(Console.ReadLine());
 
+//Cравниваем два числа и выводим результат
 if(num1 > num2){
     Console.WriteLine("Большее: " + num1);
     Console.WriteLine("Меньшее: " + num2);
@@ -15,6 +16,9 @@ else if(num1 < num2){
     Console.WriteLine("Большее: " + num2);
     Console.WriteLine("Меньшее: " + num1);
 }
+
+//Это условие необходимо для  случая равенства чисел.
 else{
     Console.WriteLine("Числа равны");
 }
+
